ae12651d5c56d9c19d6ff5605f7691118bf7d998
HViktorTsoi <cuijiahetony@163.com>
2018-05-29 19:57:54 +0800

    更换了前端路径

--- Demo/application.py
+++ Demo/application.py
@@ -1,19 +1,12 @@
 from flask import Flask
-import pymongo
+import redis
 import json
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient(host='127.0.0.1', port=27017)
-db = client.get_database(name='zhihu')
-user_col = db.get_collection('user')
-proxy_col = db.get_collection('proxy')
-relationship_col = db.get_collection('relationship')
-history_list = []
 
-
-@app.route('/zhihu_user_count')
-def index():
+@app.route('/start')
+def start():
     history_list.insert(0, [user_col.count(), relationship_col.count(), proxy_col.count()])
     return json.dumps(
         history_list,
@@ -22,4 +15,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
