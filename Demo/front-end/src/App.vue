1aac54405e2fc51ccf2bb271ea52e3ec093cb6e5
HViktorTsoi <cuijiahetony@163.com>
2018-05-29 16:01:22 +0800

    调整了配色方案 去掉了冗余函数

--- Demo/front-end/src/App.vue
+++ Demo/front-end/src/App.vue
@@ -2,20 +2,23 @@
   <div id="app">
     <el-container>
       <el-header>
-        <el-menu background-color="#2c2c2c" mode="horizontal">
+        <el-menu class="navbar" mode="horizontal">
           <el-menu-item class="navbar-brand" index="0">
             <i class="el-icon-share"></i>NME在线社交网络演化模型</el-menu-item>
         </el-menu>
       </el-header>
       <el-container>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
         <el-aside class="side-menu-container" width="200px">
-          <el-menu default-active="2">
+          <el-menu text-color="#300232" active-text-color="#c43235" default-active="2">
             <el-menu-item index="2" @click="$router.push('/evolution')">
-              <i class="el-icon-view"></i>
+              <i class="el-icon-share"></i>
               <span slot="title">模型演化分析</span>
             </el-menu-item>
             <el-menu-item index="3" @click="$router.push('/comparison')">
-              <i class="el-icon-info"></i>
+              <i class="el-icon-news"></i>
               <span slot="title">数据集拟合</span>
             </el-menu-item>
             <el-menu-item index="4" @click="$router.push('setting')">
@@ -24,9 +27,6 @@
             </el-menu-item>
           </el-menu>
         </el-aside>
-        <el-main>
-          <router-view></router-view>
-        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -48,17 +48,35 @@ body {
   margin: 0;
   padding: 0;
   .side-menu-container {
-    border-left: 1px solid #000;
+    .el-menu {
+      border-left: solid 1px #e6e6e6;
+      .el-menu-item {
+        &:hover,
+        &.is-active {
+          background-color: $clr_w !important;
+        }
+      }
+    }
   }
   .el-header {
     padding: 0;
   }
 }
-.navbar-brand {
-  color: #ffffff !important;
-  font-size: 16px;
-  i {
-    font-size: 23px !important;
+.navbar {
+  background-color: $clr_b;
+  .navbar-brand {
+    &:hover {
+      background-color: $clr_b !important;
+    }
+    &.is-active {
+      background-color: $clr_b !important;
+    }
+    color: $clr_w !important;
+    font-size: 16px;
+    i {
+      font-size: 23px !important;
+      color: $clr_r !important;
+    }
   }
 }
 </style>
--- Demo/front-end/src/main.js
+++ Demo/front-end/src/main.js
@@ -12,23 +12,6 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-// 时间转换
-Vue.prototype.$parseTime = function (unixTimestamp) {
-  function fmtSgl (num) {
-    return num >= 10 ? num : '0' + num
-  }
-  if (unixTimestamp) {
-    var date = new Date(unixTimestamp * 1000)
-    var formattedTime =
-      fmtSgl(date.getHours()) + ':' +
-      fmtSgl(date.getMinutes()) + ':' +
-      fmtSgl(date.getSeconds())
-    return formattedTime
-  } else {
-    return null
-  }
-}
-
 Vue.use(Vuex)
 Vue.use(VueRouter)
 Vue.use(ElementUI)
