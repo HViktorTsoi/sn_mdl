8cc9ae422a7f593047af1926a338078c80de4503
HViktorTsoi <cuijiahetony@163.com>
2018-05-29 15:11:17 +0800

    增加了演示系统的前端脚手架

---
+++ Demo/front-end/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <el-menu background-color="#2c2c2c" mode="horizontal">
+          <el-menu-item class="navbar-brand" index="0">
+            <i class="el-icon-share"></i>NME在线社交网络演化模型</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-container>
+        <el-aside class="side-menu-container" width="200px">
+          <el-menu default-active="2">
+            <el-menu-item index="2" @click="$router.push('/evolution')">
+              <i class="el-icon-view"></i>
+              <span slot="title">模型演化分析</span>
+            </el-menu-item>
+            <el-menu-item index="3" @click="$router.push('/comparison')">
+              <i class="el-icon-info"></i>
+              <span slot="title">数据集拟合</span>
+            </el-menu-item>
+            <el-menu-item index="4" @click="$router.push('setting')">
+              <i class="el-icon-setting"></i>
+              <span slot="title">用户设置</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  mounted: function () {
+    console.log(this.$route)
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./assets/css/common.scss";
+body {
+  margin: 0;
+  padding: 0;
+  .side-menu-container {
+    border-left: 1px solid #000;
+  }
+  .el-header {
+    padding: 0;
+  }
+}
+.navbar-brand {
+  color: #ffffff !important;
+  font-size: 16px;
+  i {
+    font-size: 23px !important;
+  }
+}
+</style>
---
+++ Demo/front-end/src/main.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import App from './App'
+
+import store from './store'
+
+Vue.config.productionTip = false
+
+// 时间转换
+Vue.prototype.$parseTime = function (unixTimestamp) {
+  function fmtSgl (num) {
+    return num >= 10 ? num : '0' + num
+  }
+  if (unixTimestamp) {
+    var date = new Date(unixTimestamp * 1000)
+    var formattedTime =
+      fmtSgl(date.getHours()) + ':' +
+      fmtSgl(date.getMinutes()) + ':' +
+      fmtSgl(date.getSeconds())
+    return formattedTime
+  } else {
+    return null
+  }
+}
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+Vue.prototype.$http = axios
+
+// 配置路由
+var routes = [
+  {
+    path: '/',
+    redirect: 'evolution'
+  },
+  {
+    path: '/evolution',
+    component: require('./components/Evolution').default,
+    name: 'evolution'
+  },
+  {
+    path: '/comparison',
+    component: require('./components/Comparison').default,
+    name: 'comparison'
+  }
+]
+var router = new VueRouter({
+  routes: routes
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App },
+  store,
+  router
+})
